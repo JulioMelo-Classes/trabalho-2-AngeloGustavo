e998c2514624200ff12fed6c8ddcafe18cef1690
AngeloGustavo <angelogustavo157@gmail.com>
2021-07-16 02:33:43 -0300

    Sets e Gets de Usuario

--- include/usuario.h
+++ include/usuario.h
@@ -1,10 +1,27 @@
 #ifndef USUARIO_H
 #define USUARIO_H
+#include <string>
+using namespace std;
 
 class Usuario{
-	/*
-		TODO implementação dos atributos e métodos
-	*/
+	public:
+		void setId(int _id);
+		int getId();
+
+		void setNome(string _nome);
+		string getNome();
+
+		void setSenha(string _senha);
+		string getSenha();
+
+		void setEmail(string _email);
+		string getEmail();
+
+	private:
+		int id;
+		string nome;
+		string senha;
+		string email;
 };
 
 #endif
