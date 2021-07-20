7e6e520a1b68ef03e561dfb6b83949fc98ffb878
AngeloGustavo <angelogustavo157@gmail.com>
2021-07-20 17:14:28 -0300

    Adicionando comandos Servidores

--- include/servidor.h
+++ include/servidor.h
@@ -1,10 +1,31 @@
 #ifndef SERVIDOR_H
 #define SERVIDOR_H
 
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class Servidor{
-	/*
-		TODO implementação dos atributos e métodos
-	*/
+	public:
+		void setDonoId(int _id);
+		int getDonoId();
+
+		void setNome(string _nome);
+		string getNome();
+
+		void setDescricao(string _descricao);
+		string getDescricao();
+
+		void setCodigo(string _codigo);
+		string getCodigo();
+	private:
+		int usuarioDonoId;
+		string nome;
+		string descricao;
+		string codigoConvite;
+		//vector<CanalTexto> canaisTexto;
+		//vector<int> participantesIDs;
 };
 
 #endif
