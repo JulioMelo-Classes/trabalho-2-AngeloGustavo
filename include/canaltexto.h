79f4b8b02c78eb8833fb3e5375e9880e1428c411
AngeloGustavo <angelogustavo157@gmail.com>
2021-08-03 02:42:16 -0300

    Adição de classe CanalTexto

--- include/canaltexto.h
+++ include/canaltexto.h
@@ -1,10 +1,22 @@
 #ifndef CANALTEXTO_H
 #define CANALTEXTO_H
 
+#include "mensagem.h"
+
+#include <vector>
+
+using namespace std;
+
 class CanalTexto{
-	/*
-		TODO implementação dos atributos e métodos
-	*/
+	private:
+		string nome;
+		vector<Mensagem> mensagens;
+	public:
+		void setNome(string _nome);
+		string getNome();
+
+		//void listMensagens(int id);
+		//void addMensagem(int id, string mensagem);
 };
 
 #endif
