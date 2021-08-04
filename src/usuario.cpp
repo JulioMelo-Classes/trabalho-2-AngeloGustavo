a6e113ef2333c168acebf80f1477f8000ad804a3
AngeloGustavo <angelogustavo157@gmail.com>
2021-08-04 02:05:04 -0300

    Substituição de Set por Construtor

--- src/usuario.cpp
+++ src/usuario.cpp
@@ -1,34 +1,28 @@
 #include "usuario.h"
-#include <iostream>
-#include <sstream>
-#include <algorithm>
 
 using namespace std;
 
-void Usuario::setId(int _id){
+Usuario::Usuario(int _id, string _nome, string _senha, string _email){
     this->id = _id;
-};
+    this->nome = _nome;
+    this->senha = _senha;
+    this->email = _email;
+}
+
 int Usuario::getId(){
     return this->id;
-};
+}
 
-void Usuario::setNome(string _nome){
-    this->nome = _nome;
-};
 string Usuario::getNome(){
     return this->nome;
-};
+}
 
-void Usuario::setSenha(string _senha){
-    this->senha = _senha;
-};
-string Usuario::getSenha(){
-    return this->senha;
-};
-
-void Usuario::setEmail(string _email){
-    this->email = _email;
-};
 string Usuario::getEmail(){
     return this->email;
-};
+}
+
+bool Usuario::checkLogin(string _email, string _senha){
+    if(this->email == _email && this->senha == _senha)
+        return true;
+    return false;
+}
