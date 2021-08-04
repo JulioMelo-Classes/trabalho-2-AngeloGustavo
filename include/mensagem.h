c9e12b79858f0ef5c09053f43388a56472105dad
AngeloGustavo <angelogustavo157@gmail.com>
2021-08-04 02:07:28 -0300

    Documentação Doxygen

--- include/mensagem.h
+++ include/mensagem.h
@@ -1,3 +1,13 @@
+/**
+ * @file mensagem.h
+ * @author Angelo Sales
+ * @brief Classe Mensagem
+ * @version 0.1
+ * @date 2021-08-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef MENSAGEM_H
 #define MENSAGEM_H
 
@@ -7,17 +17,38 @@ using namespace std;
 
 class Mensagem{
     private:
-        string dataHora;
-        int enviadaPor;
-        string conteudo;
+        string dataHora; //<! String de Data e Hora no padrão especificado no documento do projeto
+        int enviadaPor; //<! ID do autor da mensagem
+        string conteudo; //<! Conteudo da mensagem
     public:
-        void setDataHora(string _dataHora);
+        /**
+        * @brief Construtor de Mensagem
+        * 
+        * @param _dataHora 
+        * @param _enviadaPor 
+        * @param _conteudo 
+        */
+        Mensagem(string _dataHora, int _enviadaPor, string _conteudo);
+        	
+        /**
+        * @brief Retorna string de Data e Hora
+        * 
+        * @return string 
+        */
         string getDataHora();
+        	
+        /**
+        * @brief Retorna ID do autor da mensagem
+        * 
+        * @return int 
+        */
+        int getEnviadaPor();	
 
-        void setEnviadaPor(int _enviadaPor);
-        int getEnviadaPor();
-                
-        void setConteudo(string _conteudo);
+        /**
+        * @brief Retorna conteudo da mensagem
+        * 
+        * @return string 
+        */
         string getConteudo();       
 };
 
