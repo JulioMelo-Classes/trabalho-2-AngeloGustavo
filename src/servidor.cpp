16ee656c51798f99657ac241fb079ee248878512
AngeloGustavo <angelogustavo157@gmail.com>
2021-08-04 02:05:26 -0300

    Substituição de Set por Construtor

--- src/servidor.cpp
+++ src/servidor.cpp
@@ -1,4 +1,5 @@
 #include "servidor.h"
+#include "usuario.h"
 #include <iostream>
 #include <sstream>
 #include <algorithm>
@@ -7,37 +8,35 @@
 
 using namespace std;
 
-void Servidor::setDonoId(int _id){
+Servidor::Servidor(int _id, string _nome){
     this->usuarioDonoId = _id;
-};
+    this->nome = _nome;
+}
 int Servidor::getDonoId(){
     return this->usuarioDonoId;
-};
+}
 
-void Servidor::setNome(string _nome){
-    this->nome = _nome;
-};
 string Servidor::getNome(){
     return this->nome;
-};
+}
 
 void Servidor::setDescricao(string _descricao){
     this->descricao = _descricao;
-};
+}
 string Servidor::getDescricao(){
     return this->descricao;
-};
+}
 
 void Servidor::setCodigo(string _codigo){
     this->codigoConvite = _codigo;
-};
+}
 string Servidor::getCodigo(){
     return this->codigoConvite;
-};
+}
 
 void Servidor::addParticipante(int _id){
     participantesIDs.push_back(_id);
-};
+}
 void Servidor::delParticipante(int _id){//Reformular método
     auto posicao = participantesIDs.begin();
     int i=0;
@@ -47,18 +46,38 @@ void Servidor::delParticipante(int _id){//Reformular método
         i++;
     }
     participantesIDs.erase(posicao);
-};
+}
 
-vector<string> Servidor::getCanais(){
-    vector<string> saida;
+void Servidor::printCanais(){
+    if(canaisTexto.size() == 0)
+        cout<<"Não há canais para exibir";
     for(int i=0; i<canaisTexto.size(); i++){
-        saida.push_back(canaisTexto[i].getNome());
+        if(i>0)
+            cout<<endl;
+        cout<<canaisTexto[i].getNome();
     }
-    return saida;
-};
+}
 
 void Servidor::addCanal(string nome){
-    CanalTexto temp;
-    temp.setNome(nome);
+    CanalTexto temp(nome);
     canaisTexto.push_back(temp);
+}
+
+bool Servidor::checkCanal(string nome){
+    for(int i=0; i<canaisTexto.size(); i++)
+        if(canaisTexto[i].getNome() == nome)
+            return true;
+    return false;    
+}
+
+void Servidor::addMensagem_Server(int id, const string mensagem, string canal){
+    for(int i=0; i<canaisTexto.size(); i++)
+        if(canaisTexto[i].getNome() == canal)
+            canaisTexto[i].addMensagem(id, mensagem);
+}
+
+void Servidor::printMensagens_Server(string canal, vector<Usuario> usuarios){
+    for(int i=0; i<canaisTexto.size(); i++)
+        if(canaisTexto[i].getNome() == canal)
+            canaisTexto[i].printMensagens(usuarios);
 }
