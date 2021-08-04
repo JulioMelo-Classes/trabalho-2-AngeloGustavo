c3d6876ee62b6d3b5fd0fcdf66791c37bb5b69d2
AngeloGustavo <angelogustavo157@gmail.com>
2021-08-04 02:05:38 -0300

    Substituição de Set por Construtor

--- src/mensagem.cpp
+++ src/mensagem.cpp
@@ -5,23 +5,20 @@
 
 using namespace std;
 
-void Mensagem::setDataHora(string _dataHora){
+Mensagem::Mensagem(string _dataHora, int _enviadaPor, string _conteudo){
     this->dataHora = _dataHora;
+    this->enviadaPor = _enviadaPor;
+    this->conteudo = _conteudo;
 }
+
 string Mensagem::getDataHora(){
     return this->dataHora;
 }
 
-void Mensagem::setEnviadaPor(int _enviadaPor){
-    this->enviadaPor = _enviadaPor;
-}
 int Mensagem::getEnviadaPor(){
     return this->enviadaPor;
 }
         
-void Mensagem::setConteudo(string _conteudo){
-    this->conteudo = _conteudo;
-}
 string Mensagem::getConteudo(){
     return this->conteudo;
 }
